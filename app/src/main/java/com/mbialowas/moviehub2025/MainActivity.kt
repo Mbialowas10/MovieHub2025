6619d14b0d4866d872dbdeec3e34e03a1600bbb6
Michael Bialowas <mbialowas@rrc.ca>
2025-02-05 13:57:51 -0600

    BottomNavigationAdded_s249

--- app/src/main/java/com/mbialowas/moviehub2025/MainActivity.kt
+++ app/src/main/java/com/mbialowas/moviehub2025/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+import com.mbialowas.moviehub2025.Navigation.BottomNav
+import com.mbialowas.moviehub2025.destinations.Destination
 import com.mbialowas.moviehub2025.screens.MovieScreen
 import com.mbialowas.moviehub2025.screens.*
 import com.mbialowas.moviehub2025.ui.theme.MovieHub2025Theme
@@ -22,11 +31,42 @@ class MainActivity : ComponentActivity() {
         setContent {
             MovieHub2025Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
                     //MovieScreen(modifier = Modifier.padding(innerPadding))
-                    WatchScreen(modifier = Modifier.padding(innerPadding))
+                    //WatchScreen(modifier = Modifier.padding(innerPadding))
+                    App(navController = navController, modifier = Modifier.padding(innerPadding))
                 }
             }
         }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun App(navController: NavHostController, modifier: Modifier = Modifier){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("MovieHub 2025") }
+            )
+        },
+        bottomBar = { BottomNav(navController = navController) }
+    ){ paddingValues ->
+        paddingValues.calculateBottomPadding()
+        Spacer(modifier = Modifier.padding(paddingValues))
+        NavHost(
+            navController = navController as NavHostController, startDestination = Destination.Movie.route
+        ){
+            composable(Destination.Movie.route){
+                MovieScreen()
+            }
+            composable(Destination.Watch.route) {
+                WatchScreen()
+            }
+            composable(Destination.Search.route) {
+                SearchScreen()
+            }
+        }
+
     }
 }
 
---
+++ app/src/main/java/com/mbialowas/moviehub2025/destinations/Destination.kt
@@ -0,0 +1,13 @@
+package com.mbialowas.moviehub2025.destinations
+
+/*
+ Where the app should go!
+ SEALED so that we limite the number of subclasses that can
+ be dervied
+ */
+
+sealed class Destination(val route: String) {
+    object Movie :  Destination("movie")
+    object Watch :  Destination("watch")
+    object Search : Destination("search")
+}
