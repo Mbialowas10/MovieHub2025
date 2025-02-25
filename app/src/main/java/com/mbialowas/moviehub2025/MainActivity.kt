ae9e8282de2acb19f0bdd1b3c2eb351d69025403
Michael Bialowas <mbialowas@rrc.ca>
2025-02-25 11:51:21 -0600

    display the movie data on MovieDetailScreen

--- app/src/main/java/com/mbialowas/moviehub2025/MainActivity.kt
+++ app/src/main/java/com/mbialowas/moviehub2025/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mbialowas.moviehub2025
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +13,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -26,6 +31,8 @@ import com.mbialowas.moviehub2025.destinations.Destination
 import com.mbialowas.moviehub2025.screens.MovieScreen
 import com.mbialowas.moviehub2025.screens.*
 import com.mbialowas.moviehub2025.ui.theme.MovieHub2025Theme
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,15 +47,16 @@ class MainActivity : ComponentActivity() {
                     val moviesManager = MoviesManager(db)
 
 
-                    App(navController = navController, modifier = Modifier.padding(innerPadding), moviesManager)
+                    App(navController = navController, modifier = Modifier.padding(innerPadding), moviesManager, db)
                 }
             }
         }
     }
 }
+@SuppressLint("CoroutineCreationDuringComposition")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun App(navController: NavHostController, modifier: Modifier = Modifier, moviesManager: MoviesManager){
+fun App(navController: NavHostController, modifier: Modifier = Modifier, moviesManager: MoviesManager, db:AppDatabase){
     Scaffold(
         topBar = {
             TopAppBar(
@@ -71,9 +79,23 @@ fun App(navController: NavHostController, modifier: Modifier = Modifier, moviesM
             composable(Destination.Search.route) {
                 SearchScreen()
             }
-            composable(Destination.MovieDetail.route){
-                val movie = Movie(title="Fake Movie", overview="This is a much better movie than Wicked!", poster_path = "fake.png")
-                    MovieDetailScreen(modifier = Modifier.padding(paddingValues), movie=movie)
+            composable(Destination.MovieDetail.route){ navBackStackEntry ->
+                var movie by remember {
+                    mutableStateOf<Movie?>(null)
+                }
+                //val movie = Movie(title="Fake Movie", overview="This is a much better movie than Wicked!", poster_path = "fake.png")
+                val movie_id: String? = navBackStackEntry.arguments?.getString("movieID")
+                // monitor GlobalScope annotation if code breaks in future see - @SuppressLint("CoroutineCreationDuringComposition
+                GlobalScope.launch {
+                    if (movie_id != null) {
+                        movie = db.movieDoa().getMovieById( movie_id.toInt() )
+                    }
+                }
+                movie?.let{
+                    MovieDetailScreen(modifier = Modifier.padding(paddingValues), movie= movie!!)
+                }
+
+                //MovieDetailScreen(modifier = Modifier.padding(paddingValues), movie=movie)
 
             }
         }
