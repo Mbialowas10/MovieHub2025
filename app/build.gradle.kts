16456a17db40af5905d0e21f3a5ad54c9d95e428
Michael Bialowas <mbialowas@rrc.ca>
2025-04-01 11:48:47 -0500

    Setup_gMaps_s251

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,24 @@ android {
 
 dependencies {
 
+    //google maps
+    implementation(libs.play.services.maps.v1820)  // Google Maps
+    implementation(libs.play.services.location) // Location Services
+    implementation(libs.places.v330) // Places API
+
+    // accompanist permissions
+    implementation(libs.accompanist)
+
+    // google maps composable
+    implementation(libs.google.maps.sdk) // Google Maps SDK
+    implementation(libs.maps.compose)    // Compose Maps Library
+    implementation (libs.androidx.compose.material) // Had to add bc or Marker composable
+
+    // google play services
+    implementation(libs.play.services.maps)
+
+
+
     // moshi
     implementation(libs.moshi.kotlin)
     implementation(libs.converter.moshi)
